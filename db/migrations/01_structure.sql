862e0ee154dba2e19f49a4d7b872cc527ab428c4
Terence Martin <odatnurd@gmail.com>
2025-02-10 20:38:32 -0600

    Initial in progress DB setup
    This implements the full database schema as currently envisioned; in the
    process a few fields were manipulated and added as things clarified at
    the data level.
    
    More work may still be required, but for the moment we're in a good
    place.

--- db/migrations/01_structure.sql
+++ db/migrations/01_structure.sql
@@ -1,1 +1,314 @@
--- Create all tables and their indexes
+--------------------------------------------------------------------------------
+--- C L E A N U P --------------------------------------------------------------
+--------------------------------------------------------------------------------
+
+
+DROP TABLE IF EXISTS FilamentParameterMappings;
+DROP TABLE IF EXISTS FilamentParameters;
+
+DROP TABLE IF EXISTS FilamentSpools;
+DROP TABLE IF EXISTS InventoryLocations;
+DROP TABLE IF EXISTS PurchaseList;
+
+DROP TABLE IF EXISTS Users;
+DROP TABLE IF EXISTS Roles;
+
+DROP TABLE IF EXISTS Barcodes;
+DROP TABLE IF EXISTS Filaments;
+DROP TABLE IF EXISTS FilamentBrands;
+DROP TABLE IF EXISTS FilamentMaterials;
+DROP TABLE IF EXISTS Spools;
+
+
+--------------------------------------------------------------------------------
+-- U S E R   I N F O R M A T I O N ---------------------------------------------
+--------------------------------------------------------------------------------
+
+
+-- Each user that is known to the system has a role assigned to them that
+-- outlines how different parts of the system act towards that user.
+--
+-- Each role has a numeric access level assigned to it, which is what is used to
+-- actually control the role system; the lower the value, the higher the access
+-- level.
+CREATE TABLE IF NOT EXISTS Roles (
+    roleId TEXT UNIQUE PRIMARY KEY,
+    name TEXT NOT NULL,
+
+    accessLevel INTEGER NOT NULL
+);
+
+
+--------------------------------------------------------------------------------
+
+
+-- Each user that is known to the system is tracked by an entry in this table.
+--
+-- Internally, each user has a unique nanoid identifier created for them at the
+-- time they are added to the table.
+--
+-- Externally, we use Acheron as our authentication back end. Acheron supports
+-- multiple authentication back ends, so the table here tracks the unique keys
+-- that are provided to us, allowing for a potential mixed authentication system
+-- to be used.
+--
+-- Currently the disambiguating factor is the Acheron information, which means
+-- that logging in via different methods can cause the same user to appear twice
+-- in the list even if their other details are the same. This is because not all
+-- supported back ends guarantee the email address will be set.
+--
+-- Runtime support could be added to merge users together if it was known they
+-- were the same user logging in via multiple methods.
+CREATE TABLE IF NOT EXISTS Users (
+    userId TEXT UNIQUE PRIMARY KEY,
+    role TEXT NOT NULL REFERENCES Roles(roleId) DEFAULT('rVBBPOUWqPbWBmiUQuwGs'),
+
+    acheronId TEXT NOT NULL,
+    acheronProvider TEXT TEXT NOT NULL,
+
+    name TEXT NOT NULL,
+    email TEXT NOT NULL DEFAULT(''),
+    profileImage TEXT NOT NULL DEFAULT('')
+);
+
+-- We need to be able to look up a user not only by their ID, but also by the
+-- external Acheron authentication method, so that we can cross associate the
+-- two.
+CREATE UNIQUE INDEX idx_auth_users on Users(acheronId, acheronProvider);
+
+
+--------------------------------------------------------------------------------
+-- G L O B A L   I N F O R M A T I O N -----------------------------------------
+--------------------------------------------------------------------------------
+
+
+-- This table holds the global global list of all known filament brands.
+CREATE TABLE IF NOT EXISTS FilamentBrands (
+    brandId TEXT UNIQUE PRIMARY KEY,
+
+    name TEXT UNIQUE NOT NULL,
+    homepage TEXT DEFAULT('')
+);
+
+
+CREATE UNIQUE INDEX idx_brand_names on FilamentBrands(name);
+
+
+--------------------------------------------------------------------------------
+
+
+-- This table holds the global global list of all known filament materials.
+CREATE TABLE IF NOT EXISTS FilamentMaterials (
+    materialId TEXT UNIQUE PRIMARY KEY,
+
+    name TEXT UNIQUE NOT NULL
+);
+
+
+CREATE UNIQUE INDEX idx_material_names on FilamentMaterials(name);
+
+
+--------------------------------------------------------------------------------
+
+
+-- This table holds the basic core information for a specific type of filament,
+-- as described by its brand, material, color, diameter, and so on.
+CREATE TABLE IF NOT EXISTS Filaments (
+    filamentId TEXT UNIQUE PRIMARY KEY,
+
+    image TEXT DEFAULT(''),
+
+    brandId TEXT NOT NULL REFERENCES FilamentBrands(brandId),
+    materialId TEXT NOT NULL REFERENCES FilamentMaterials(materialId),
+    color TEXT NOT NULL,
+    diameter FLOAT NOT NULL DEFAULT(1.75),
+
+    reorderURL TEXT DEFAULT(null)
+);
+
+
+CREATE INDEX idx_filament_brand_material on Filaments(brandId, materialId);
+
+
+--------------------------------------------------------------------------------
+
+
+-- This table holds the basic information on spools that can hold filament; each
+-- can have a weight that they weigh with no filament on them, and can able be
+-- marked as reusable, in which case the system will not discard them from a
+-- user's inventory automatically.
+CREATE TABLE IF NOT EXISTS Spools (
+    spoolId TEXT UNIQUE PRIMARY KEY,
+
+    brandId TEXT NULL REFERENCES FilamentBrands(brandId),
+
+    name TEXT NOT NULL,
+    description TEXT NOT NULL DEFAULT(''),
+
+    emptyWeightGrams INTEGER NOT NULL DEFAULT(0),
+
+    -- Spools that are reusable in theory can be purchased, so they have a
+    -- reorder URL. Spools that are disposable are not purchasable, so there will
+    -- never be a reorder URL for them.
+    reusable BOOL NOT NULL DEFAULT(0),
+    reorderURL TEXT DEFAULT('')
+);
+
+
+CREATE INDEX idx_spool_info on Spools(name, reusable);
+
+
+--------------------------------------------------------------------------------
+
+
+-- A list of product barcodes known to the system, and the filament or spool
+-- that the apply to.
+
+-- A barcode always associates with EITHER a specific filamentId, OR a specific
+-- spoolId, but never both.
+
+-- In the first case, the barcode is for products that are refills or filament
+-- that ships directly on its own recyclable spool.
+--
+-- The second case is for things like Bambu reusable spools, which on their own
+-- have a bar code that you can use to look them up.
+--
+-- The reason you can't have both is because something can't be a filament and a
+-- spool at the same time.
+CREATE TABLE IF NOT EXISTS Barcodes (
+    code TEXT UNIQUE PRIMARY KEY,
+
+    -- These two fields are mutually exclusive, since only one product can have
+    -- a barcode of a specific value.
+    filamentId TEXT REFERENCES Filaments(filamentId),
+    spoolId TEXT REFERENCES Spools(spoolId)
+);
+
+
+--------------------------------------------------------------------------------
+-- F I L A M E N T   C O N F I G U R A T I O N ---------------------------------
+--------------------------------------------------------------------------------
+
+
+-- This table implements an open ended filament parameter list that allows both
+-- the system and specific users to apply any sort of user specific data they
+-- would like to a filament.
+CREATE TABLE IF NOT EXISTS FilamentParameters (
+    parameterId TEXT UNIQUE PRIMARY KEY,
+
+    name TEXT NOT NULL,
+    type TEXT NOT NULL CHECK(type IN ('text', 'int', 'float', 'bool', 'date', 'time', 'datetime')),
+
+    text_value TEXT DEFAULT(''),
+    int_value INTEGER DEFAULT(0),
+    float_value FLOAT DEFAULT(0.0),
+    bool_value BOOLEAN DEFAULT(0),
+    date_value DATE DEFAULT(''),
+    time_value TIME DEFAULT(''),
+    datetime_value DATETIME DEFAULT('')
+);
+
+CREATE INDEX idx_parameter_names on FilamentParameters(name);
+
+
+--------------------------------------------------------------------------------
+
+
+-- Create a relational mapping between filament parameters and actual filaments.
+--
+-- Each mapping can either be global or user specific, depending on the value of
+-- the given userId.
+CREATE TABLE IF NOT EXISTS FilamentParameterMappings (
+    mappingId TEXT UNIQUE PRIMARY KEY,
+
+    -- userId can be null, in which case this mapping applies globally to all
+    -- users; otherwise this mapping is for only that specific user, and can
+    -- override parameters of the same name that are global.
+    userId TEXT REFERENCES Users(UserId),
+    filamentId TEXT NOT NULL REFERENCES Filaments(filamentId),
+    parameterId TEXT NOT NULL REFERENCES FilamentParameters(parameterId)
+);
+
+
+CREATE INDEX idx_filament_parameter_mapping on FilamentParameterMappings(userId, filamentId, parameterId);
+
+
+--------------------------------------------------------------------------------
+-- U S E R   I N F O R M A T I O N ---------------------------------------------
+--------------------------------------------------------------------------------
+
+
+-- This table implements a simple inventory tagging system that allows a user
+-- specific, fully open ended naming system to name inventory locations. Each
+-- item that is owned by a user can be marked as being in one of the specific
+-- locations.
+CREATE TABLE IF NOT EXISTS InventoryLocations (
+    inventoryId TEXT UNIQUE PRIMARY KEY,
+
+    userId TEXT NOT NULL REFERENCES Users(UserId),
+    name TEXT NOT NULL
+);
+
+
+CREATE INDEX idx_inventory_names on InventoryLocations(userId, name);
+
+
+--------------------------------------------------------------------------------
+
+
+-- This table implements a wishlist/purchase list of sorts that allows a
+-- specific user to track the filaments and/or spools that they would like to
+-- purchase more of. This can be auto-populated as a part of marking that a
+-- specific filament is now fully used up as well as by an automated check that
+-- adds more of a filament when its available level goes below a threshold.
+CREATE TABLE IF NOT EXISTS PurchaseList (
+    purchaseId TEXT UNIQUE PRIMARY KEY,
+
+    userId TEXT NOT NULL REFERENCES Users(UserId),
+
+    -- These two fields are mutually exclusive, since you either want to buy
+    -- filament and it comes on a spool already, or you want to buy a reusable
+    -- spool to put filament on later.
+    filamentId TEXT REFERENCES Filaments(filamentId),
+    spoolId TEXT REFERENCES Spools(spoolId),
+
+    count INTEGER NOT NULL DEFAULT(1)
+);
+
+
+CREATE INDEX idx_user_filament_purchase_list on PurchaseList(userId, filamentId);
+CREATE INDEX idx_user_spool_purchase_list on PurchaseList(userId, spoolId);
+
+
+--------------------------------------------------------------------------------
+
+
+-- This table tracks the user specific list of filaments and spools that a
+-- specific user has at any given time. The entries hdere can contain either a
+-- filament and a spoolId, or just a spoolId, depending on whether or not this
+-- is tracking a specific kind of filament on a known spool type, or just an
+-- empty reusable spool that currently has no filament on it.
+CREATE TABLE IF NOT EXISTS FilamentSpools (
+    filamentSpoolId TEXT UNIQUE PRIMARY KEY,
+
+    userId TEXT NOT NULL REFERENCES Users(UserId),
+    filamentId TEXT REFERENCES Filaments(filamentId),
+    spoolId TEXT NOT NULL REFERENCES Spools(spoolId),
+
+    inventoryId TEXT NULL REFERENCES InventoryLocations(inventoryId),
+
+    notes TEXT NOT NULL DEFAULT(''),
+
+    purchaseDate DATETIME NOT NULL DEFAULT(''),
+    purchasePrice FLOAT NOT NULL DEFAULT(0.0),
+
+    spoolAdjustWeightGrams INTEGER NOT NULL DEFAULT(0),
+    filamentWeightGrams INTEGER NOT NULL DEFAULT(0)
+);
+
+
+CREATE INDEX idx_user_spools on FilamentSpools(userId, spoolId, filamentId);
+CREATE INDEX idx_user_filaments on FilamentSpools(userId, filamentId);
+
+
+--------------------------------------------------------------------------------
--- db/migrations/02_bootstrap.sql
+++ db/migrations/02_bootstrap.sql
@@ -1,1 +1,170 @@
--- Populate all base data into the system
+--------------------------------------------------------------------------------
+-- D A T A   B O O T S T R A P P I N G -----------------------------------------
+--------------------------------------------------------------------------------
+
+
+-- Set up a distinct list of known roles in the system.
+--   * "admin" has full site admin rights
+--   * "superuser" is as a regular user, but with some extra privileges like
+--                 having an account that does not expire.
+--   * "user" is a regular user
+-- Set up the distinct list of known roles in the system.
+INSERT INTO Roles(roleId, name, accessLevel)
+  VALUES
+    ('SctYBZqwLgiuNgZgkATsZ', 'admin',     0),
+    ('NJYiqxe2rkiFuAmu3DJDD', 'superuser', 1),
+    ('rVBBPOUWqPbWBmiUQuwGs', 'user',      2)
+  ;
+
+
+-- Insert an initial user to be the administrator. Unless you're the person that
+-- wrote this software, you probably want to change this if you run your own
+-- version, unless you'd like OdatNurd to be an administrator on your site.
+INSERT INTO Users(userId, role, acheronId, acheronProvider, name, email, profileImage )
+  VALUES
+    ('XHmX-SwOs-r-CVfbFpDq3', 'SctYBZqwLgiuNgZgkATsZ',
+        'google-oauth2|109350420655042271173', 'auth0',
+        'odatnurd', 'odatnurd@gmail.com', 'https://lh3.googleusercontent.com/a/ACg8ocIe0mPwlIlqv2c85uUtYhsIBG94kLg4HFWDmFf2jPwr8w6f6hPf=s96-c')
+    ;
+
+
+--------------------------------------------------------------------------------
+
+
+INSERT INTO FilamentBrands(brandId, name, homepage)
+  VALUES
+    ('5gkPoGB-bsk_xtaCAlYKz', 'Bambu Labs', 'https://bambulab.com/')
+  ;
+
+INSERT INTO FilamentMaterials(materialId, name)
+  VALUES
+    ('JUQw5GdDNk_zWafvIrLRR', 'PLA Basic'),
+    ('ysx2lBqIlBejCqFz26c2b', 'PLA Silk+'),
+    ('3PfahgmpuWVNbQ2tml-Xk', 'PETG'),
+    ('YmOs13GibiQK2SOq_1BDe', 'ASA'),
+    ('713OTFwPuNmyYt72K5rgr', 'TPU')
+  ;
+
+INSERT INTO Filaments(filamentId, image, brandId, materialId, color, diameter, reorderURL)
+  VALUES
+    -- PLA Basic, Black, Bambu Labs
+    ('yh3vhxJuMqv-OazNd6BD5', 'https://store.bblcdn.com/s5/default/31f2cbb9d9bc473894322fa464ea63d2.png', '5gkPoGB-bsk_xtaCAlYKz', 'JUQw5GdDNk_zWafvIrLRR', 'Black', 1.75, 'https://ca.store.bambulab.com/products/pla-basic-filament?id=43766892298480'),
+
+    -- PLA Basic, Jade White, Bambu Labs
+    ('swmUPd2uxzXhtotJDMzw8', 'https://store.bblcdn.com/s5/default/7970a11e9efd4d7bbf844311b4d762d0/White.jpg__op__resize,m_lfit,w_750__op__format,f_auto__op__quality,q_80', '5gkPoGB-bsk_xtaCAlYKz', 'JUQw5GdDNk_zWafvIrLRR', 'Jade White', 1.75, 'https://ca.store.bambulab.com/products/pla-basic-filament?id=43115681841392')
+  ;
+
+INSERT INTO Spools(spoolId, brandId, name, description, emptyWeightGrams, reusable, reorderURL)
+  VALUES
+    -- Actual weight of the cardboard spindle + the rfid tag is 29g
+    ('1lHaAWa2M8XR25g9SBcWW', '5gkPoGB-bsk_xtaCAlYKz', 'Bambu Refill', 'Bambu Filament Refill', 29, 0, ''),
+
+    -- Actual weight of the spool is 216g, but the value here also factors in the weight of the cardboard spindle.
+    ('hoJoywe1Uj7MAkPQrItL5', '5gkPoGB-bsk_xtaCAlYKz', 'Bambu Reusable', 'ABS Temp <= 70℃', 245, 1, 'https://ca.store.bambulab.com/products/bambu-reusable-spool'),
+
+    -- Actual weight of the spool is 223g, but the value here also factors in the weight of the cardboard spindle.
+    ('c-48y5CIebKYdovx9nUCr', '5gkPoGB-bsk_xtaCAlYKz', 'Bambu Reusable (High Temperature)', 'ABS+PC Temp <= 90℃', 252, 1, 'https://ca.store.bambulab.com/products/bambu-high-temperature-reusable-spool')
+  ;
+
+INSERT INTO Barcodes(code, filamentId, spoolId)
+  VALUES
+    -- PLA Basic, Black, on spool
+    ('6975337031383', 'yh3vhxJuMqv-OazNd6BD5', NULL),
+
+    -- PLA Basic, White, Bambu Refill, on spool
+    ('6975337031024', 'swmUPd2uxzXhtotJDMzw8', NULL),
+
+    -- PLA Basic, Black, Bambu Refill
+    ('6975337031178', 'yh3vhxJuMqv-OazNd6BD5', NULL),
+
+    -- It goes without saying (one hopes) that the barcode here is a fake
+    -- placeholder since I don't have access to the packaging for this at the
+    -- moment.
+    --
+    -- High Temperature Bambu Reusable Spool
+    ('1234567890123', NULL, 'c-48y5CIebKYdovx9nUCr')
+  ;
+
+
+--------------------------------------------------------------------------------
+
+
+-- Some simple test parameters for Bambu PLA Basic Black
+INSERT INTO FilamentParameters(parameterId, name, type, int_value)
+  VALUES
+    ('YXg88pps7mkPyq4yvesIr', 'Nozzle Temp. Min', 'int', 190),
+    ('Qx_ACu2hixU8gnbVYdCcd', 'Nozzle Temp. Max', 'int', 230),
+    ('X0Zl8M68v73jzySZpT4L0', 'Bed Temp. Min',    'int', 35),
+    ('jebfwIVq_F550R6VU9xLm', 'Bed Temp. Max',    'int', 45),
+    ('2oBsLSwXddUJ3qKm_wg8V', 'Printing Speed',   'int', 300)
+  ;
+
+
+-- Mapping all of the above to PLA Basic Black and PLA Basic White
+--
+-- Here the two filaments are sharing the same filament parameter; this allows
+-- a new filament to be set up as having the same properties as an existing one
+-- by just gathering all of the entries for a base filament and then re-entering
+-- them.
+--
+-- This would operate as COPY-ON-WRITE; when a modification is mode to a param,
+-- create a new parameter for the new value and update the mapping record,
+-- making sure to clean up the old one if it's no longer used (or if it's only
+-- used once, just modify it in place maybe?)
+INSERT INTO FilamentParameterMappings(mappingId, filamentId, parameterId)
+  VALUES
+    -- Bambu Basic Black PLA
+    ('jBQuSOe7lcCa6TpYbbcEP', 'yh3vhxJuMqv-OazNd6BD5', 'YXg88pps7mkPyq4yvesIr'),
+    ('hhb75G-6jAWjFAW_5rEpm', 'yh3vhxJuMqv-OazNd6BD5', 'Qx_ACu2hixU8gnbVYdCcd'),
+    ('siaff5yzGbAEMJLjKe0Qr', 'yh3vhxJuMqv-OazNd6BD5', 'X0Zl8M68v73jzySZpT4L0'),
+    ('6PPEHiE_OS0leb1nVGpe1', 'yh3vhxJuMqv-OazNd6BD5', 'jebfwIVq_F550R6VU9xLm'),
+    ('WscAJAsqfjqKdDahPuDf-', 'yh3vhxJuMqv-OazNd6BD5', '2oBsLSwXddUJ3qKm_wg8V'),
+
+     -- Bambu Basic White PLA
+    ('gzwRxFHEiTS3vhXa0ewo6', 'swmUPd2uxzXhtotJDMzw8', 'YXg88pps7mkPyq4yvesIr'),
+    ('SVAXRtxfOshTd14vyoHRd', 'swmUPd2uxzXhtotJDMzw8', 'Qx_ACu2hixU8gnbVYdCcd'),
+    ('mhzDEomHDAPWuBUKmK3vQ', 'swmUPd2uxzXhtotJDMzw8', 'X0Zl8M68v73jzySZpT4L0'),
+    ('s04Hj5vpA7EwDcYKaYOoA', 'swmUPd2uxzXhtotJDMzw8', 'jebfwIVq_F550R6VU9xLm'),
+    ('LQy2pj6XG3GzxdLfSsxr8-', 'swmUPd2uxzXhtotJDMzw8', '2oBsLSwXddUJ3qKm_wg8V')
+  ;
+
+
+--------------------------------------------------------------------------------
+
+
+INSERT INTO InventoryLocations(inventoryId, userId, name)
+  VALUES
+    ('5UNjY6Jlckl9bN_cFnzz9', 'XHmX-SwOs-r-CVfbFpDq3', 'Storage Box 1'),
+    ('kdgGXhsaAeci-RaQA0GHl', 'XHmX-SwOs-r-CVfbFpDq3', 'Storage Box 2'),
+    ('vl30Ke5n2GMcNVn8SQ075', 'XHmX-SwOs-r-CVfbFpDq3', 'AMS')
+  ;
+
+
+--------------------------------------------------------------------------------
+
+
+INSERT INTO PurchaseList(purchaseId, userId, filamentId, spoolId, count)
+  VALUES
+    -- Bambu Labs Basic Black PLA
+    ('ojQ5DvnuGXaX79kvuq1Ra', 'XHmX-SwOs-r-CVfbFpDq3', 'yh3vhxJuMqv-OazNd6BD5', NULL, 2),
+    ('mgLnH_dnBE15rO4MGgIIz', 'XHmX-SwOs-r-CVfbFpDq3', NULL, 'c-48y5CIebKYdovx9nUCr', 69)
+  ;
+
+
+--------------------------------------------------------------------------------
+
+
+INSERT INTO FilamentSPools(filamentSpoolId, userId, filamentId, spoolId, inventoryId, filamentWeightGrams)
+  VALUES
+    -- Bambu Labs Basic Black PLA in the AMS
+    ('lLaoV5rmJkPkVMKtEQsaG', 'XHmX-SwOs-r-CVfbFpDq3', 'yh3vhxJuMqv-OazNd6BD5', 'hoJoywe1Uj7MAkPQrItL5', 'XHmX-SwOs-r-CVfbFpDq3', 750),
+
+    -- Bambu Labs Basic White PLA in box 1 (refill)
+    ('OgCRmrGn_QEN96n1E8tFI', 'XHmX-SwOs-r-CVfbFpDq3', 'swmUPd2uxzXhtotJDMzw8', '1lHaAWa2M8XR25g9SBcWW', 'XHmX-SwOs-r-CVfbFpDq3', 1000),
+
+    -- Bambu Labs reusable spool in box 2
+    ('Y60HLI1LIqRchYmWQ7Zvv', 'XHmX-SwOs-r-CVfbFpDq3', NULL, 'c-48y5CIebKYdovx9nUCr', 'XHmX-SwOs-r-CVfbFpDq3', 0)
+  ;
+
+
+--------------------------------------------------------------------------------
